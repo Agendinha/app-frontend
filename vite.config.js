a2dbfc697c7deccae4a4d5afc7bc4cf709abcecf
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-04-22 20:39:54 -0300

    :wrench: Config: Configs de tailwind e shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'url';
 
-// https://vitejs.dev/config/
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
