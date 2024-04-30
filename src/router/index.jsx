df2b3c0bb7842a99d8ee2f322274fd3d4fef32bf
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-04-29 22:30:29 -0300

    :sparkles: feat: Registro

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import {BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Dashboard from '../pages/dashboard';
 import Login from '../pages/Login/login';
-import Register from '../pages/auth/Register';
+import Register from '../pages/Register/register';
 
 function MainRouter() {
   return (
@@ -10,7 +10,7 @@ function MainRouter() {
       <Routes>
         <Route path="/" element={<Home />} Component={Home}/>
         <Route path="/login" element={<Login />} Component={Login}/>
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} Component={Register}/>
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
