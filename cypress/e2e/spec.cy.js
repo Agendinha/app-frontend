48eef8cc233769fbf4681c07dd88dde1a010b91d
diogocastros1 <diogoacsantos93@gmail.com>
2024-05-25 08:53:07 -0300

    test: New tests in register

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,8 @@
+/// <reference types="cypress"/>
+
 describe('Testes de login:', () => {
   it('E-mai ou senha invalidos', () => {
-    // TODO: Colocar a URL da aplicação
-    cy.visit('http://localhost:5173/')
+    cy.visit('https://app.agendinha.online/')
     cy.get('#email').type('hello@cypress.io')
     cy.get('#password').type('123')
     cy.get('.bg-orange-400 > .flex').click()
@@ -9,12 +10,47 @@ describe('Testes de login:', () => {
     cy.get('.swal-text').should('have.text', 'Email ou senha inválidos!')
   })
   it('Login com sucesso!', () => {
-    // TODO: Colocar a URL da aplicação
-    cy.visit('http://localhost:5173/')
+    cy.visit('https://app.agendinha.online/')
     cy.get('#email').type('teste@teste.com')
     cy.get('#password').type('teste')
     cy.get('.bg-orange-400 > .flex').click()
     cy.get('button').click()
-    cy.get('.text-xl').should('have.text', 'Login bem sucedido!')
+    cy.get('.text-xl').should('have.text', 'Logout feito com sucesso!')
+  })
+  
+})
+
+describe('Testes de cadastro:', () => {
+  it('Cadastro de parceiro com sucesso!', () => {
+    cy.visit('https://app.agendinha.online/')
+    cy.get('a').click()
+    criarUsuario('Parceiro')    
+  })
+  it('Cadastro de cliente com sucesso!', () => {
+    cy.visit('https://app.agendinha.online/')
+    cy.get('a').click()
+    criarUsuario('Cliente')    
   })
 })
+
+function criarUsuario(type) {
+  let horas = new Date().getHours().toString()
+  let minutos = new Date().getMinutes().toString()
+  let seg = new Date().getSeconds().toString()
+  let nome = "Usuario" + horas + minutos + seg
+  let email = "email" + horas + minutos + seg + "@example.com"
+  let senha = horas + minutos + seg + 'Senha@'
+  let infoUsuario = {nome, email, senha}
+
+  cy.get('#nomeCompleto').type(nome)
+  cy.get('#email').type(email)
+  cy.get('#password').type(senha)
+  cy.get('#confirmPassword').type(senha)
+  cy.get('#phone').type('123456789')
+  cy.get('#userType').select(type)
+  cy.get('.bg-orange-400 > .flex').click()
+  cy.get('.swal-title').should('have.text', 'Sucesso')
+
+  return infoUsuario
+
+}
