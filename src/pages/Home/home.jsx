86192b7983ba1150dfd9e87de2d0ee3257a0a22d
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-06-03 16:39:44 -0300

    Background color

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-orange-200 text-black">
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-orange-50 text-black">
       <h1>Home</h1>
     </div>
 
--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -46,7 +46,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-orange-200 text-black">
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-orange-50 text-black">
       <Logo />
       <div className="flex flex-col w-5/12">
         <form className="flex flex-col" onSubmit={handleLogin}>
